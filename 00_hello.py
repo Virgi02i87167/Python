23112bacca674c5e8e9a3752238105672421d57b
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2025-12-22 12:55:11 -0600

    cambios

--- 00_hello.py
+++ 00_hello.py
@@ -8,3 +8,5 @@ print(type(5)) # Tipo 'int'
 print(type(1.5)) # Tipo 'float'
 print(type(3 + 1j)) # Tipo 'complex'
 print(type(True)) # Tipo 'bool'
+
+print(type(print("Mi cadena de texto"))) # Tipo 'NoneType
--- 01_variables.py
+++ 01_variables.py
@@ -5,7 +5,39 @@ print(mi_variable)
 my_int_variable = 5
 print(my_int_variable)
 
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
 my_bool_variable = False
 print(my_bool_variable)
 
-print(mi_variable, my_int_variable)
+# Concatenacion de variables en un print
+print(mi_variable, my_int_to_str_variable, my_bool_variable)
+print("Este es el valor de:", my_bool_variable)
+
+# Funciones del sistema
+print(len(mi_variable))
+
+# Variables en una sola linea
+name, surname, alias, age = "Jona", "Alfaro", "Braulio0821", 24
+print("Me llamo:", name, surname, "Mi edad es", age, "Y mi alias es:", alias)
+
+# Inputs
+"""
+name = input('¿Cual es tu nombre? ')
+age = input('¿Cual es tu edad? ')
+print(name)
+print(age)
+"""
+
+# Cambiamos el tipo
+name = 24
+age = "Jon"
+print(name)
+print(age)
+
+# Forzamos el tipo
+address: str = "Mi direccion"
+address = 32
+print(address)
