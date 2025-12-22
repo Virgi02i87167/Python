b97826d48539edd37969740f9931848625fb1740
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2025-12-22 13:50:03 -0600

    operadores

--- 02_operadores.py
+++ 02_operadores.py
@@ -1,1 +1,44 @@
 # Operadores
+
+# Operadores aritmeticos
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 // 1 // 4)
+
+print("Hola " + "Python " + "¿Que tal?")
+print("Hola " + str(5))
+print("Hola " * 5)
+print("Hola " * (2 ** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+# Operadores Comparativos
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+# Compara en ordenacion alfabetica
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "Python")
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+print("aaaa" >= "abaa") # por ASSCII
+print(len("aaaa") >= len("aaaa")) # Cuenta caracteres
+
+# Operadores Logicos
+print(3 > 4 and "Hola" > "Python")
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(not(3 > 4 ))
